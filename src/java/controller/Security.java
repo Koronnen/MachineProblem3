3e700d119cadfa3d97e8097acb3ed687736990d4
Pimentel <vinzjairus.pimentel.cics@ust.edu.ph>
2026-04-18 22:09:17 +0800

    Revised hardcoded values

--- src/java/controller/Security.java
+++ src/java/controller/Security.java
@@ -9,11 +9,15 @@ public class Security {
     public static String encrypt(String strToEncrypt, ServletContext context) {
         //Get key
         String rawKey = context.getInitParameter("SecurityKey");
+        String cipherAlg = context.getInitParameter("CipherAlgorithm");
+        String cipherMode = context.getInitParameter("CipherMode");
+        String cipherPadding = context.getInitParameter("CipherPaddingScheme");
+        String rawCipher = cipherAlg + "/" + cipherMode + "/" + cipherPadding;
         final byte[] key = rawKey.getBytes();
         String encryptedString = null;
         try {
-                Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-                final SecretKeySpec secretKey = new SecretKeySpec(key, "AES");
+                Cipher cipher = Cipher.getInstance(rawCipher);
+                final SecretKeySpec secretKey = new SecretKeySpec(key, cipherAlg);
                 cipher.init(Cipher.ENCRYPT_MODE, secretKey);
                 encryptedString = Base64.encodeBase64String(cipher.doFinal(strToEncrypt.getBytes()));
         } catch (Exception e) {
@@ -25,11 +29,15 @@ public class Security {
     public static String decrypt(String codeDecrypt, ServletContext context){
         //Get key
         String rawKey = context.getInitParameter("SecurityKey");
+        String cipherAlg = context.getInitParameter("CipherAlgorithm");
+        String cipherMode = context.getInitParameter("CipherMode");
+        String cipherPadding = context.getInitParameter("CipherPaddingScheme");
+        String rawCipher = cipherAlg + "/" + cipherMode + "/" + cipherPadding;
         final byte[] key = rawKey.getBytes();
         String decryptedString = null;
         try{
-                Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
-                final SecretKeySpec secretKey = new SecretKeySpec(key, "AES");
+                Cipher cipher = Cipher.getInstance(rawCipher);
+                final SecretKeySpec secretKey = new SecretKeySpec(key, cipherAlg);
                 cipher.init(Cipher.DECRYPT_MODE, secretKey);
                 decryptedString = new String(cipher.doFinal(Base64.decodeBase64(codeDecrypt)));
         } catch (Exception e) {
